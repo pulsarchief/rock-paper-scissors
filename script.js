846cf0ab9128b93f45ed09960964648cee71b69f
pulsarchief <145131830+pulsarchief@users.noreply.github.com>
2024-07-17 18:11:57 +0530

    added lot of css and UI added several html as well js is partly done main funtion is remaining

--- script.js
+++ script.js
@@ -1,111 +1,58 @@
-function getComputerChoice()
-{
-    let choice = Math.floor(Math.random() * 3);
-    let choice_array = ["STONE", "PAPER", "SCISSORS"];
-    return choice_array[choice];
-}
+const user_choice = document.querySelector("#choice_c");
+const comp_choice = document.querySelector("#choice_u");
 
-function playRound(user_Selection,computer_Selection)
-{   
-    switch (user_Selection.toUpperCase())
-    {
-        case "STONE":
-            switch (computer_Selection.toUpperCase())
-            {   
-                case "STONE":
-                    return 1;
-                case "PAPER":
-                    return 2;
-                case "SCISSORS":
-                    return 3;
+const user_score = document.querySelector("#user_score");
+const comp_score = document.querySelector("#comp_score");
+const info = document.querySelector("#info");
+
+const paper = document.querySelector("#paper");
+const stone = document.querySelector("#stone");
+const scissors = document.querySelector("#scissors");
+
+stone.addEventListener("click", play(1));
+paper.addEventListener("click", play(2));
+scissors.addEventListener("click", play(3));
+
+function play(user) {
+    let choices = ['stone', 'paper', 'scissors'];
+    let user_choice = user;
+    let comp_choice = Math.floor(Math.random() * 3) + 1;
+
+    switch (user_choice) {
+        case 1:
+            switch (comp_choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
             }
-        case "PAPER":
-            switch (computer_Selection.toUpperCase())
-            {   
-                case "STONE":
-                    return 3;
-                case "PAPER":
-                    return 1;
-                case "SCISSORS":
-                    return 2;
+        case 2:
+            switch (comp_choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
             }
-        case "SCISSORS":
-            switch (computer_Selection.toUpperCase())
-            {   
-                case "STONE":
-                    return 2;
-                case "PAPER":
-                    return 3;
-                case "SCISSORS":
-                    return 1;
+        case 3:
+            switch (comp_choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
             }
         default:
-            alert("User please give proper input:")
-    }
-}
-
-function game()
-{
-    let computer_Selection = getComputerChoice();
-    let user_Selection = prompt("Please choose between STONE,PAPER and SCISSORS:").toUpperCase();
-    let twl = (playRound(user_Selection, computer_Selection));
-    console.log(twl);
-    switch (twl)
-    {
-        case 1:
-            alert("TIE!!!! both played "+user_Selection);
-            game();
-            break;
-        case 2:
-            alert("USER LOSES!!!! "+computer_Selection+" beats "+user_Selection);
-            comp+=1;
-            break;
-        case 3:
-            alert("USER WINS!!!! "+user_Selection+" beats "+computer_Selection);
-            user += 1;
             break;
     }
 }
-let comp=0, user = 0;
-
-function rounds()
-{
-    alert("Welcome to STONE,PAPER,SCISSORS")
-    alert("User plays first:")
-    while ((comp < 5) && (user < 5))
-    {
-        game(comp, user);
-        alert("User : " + user + " Computer : " + comp);
-    }
-    if (comp == 5)
-    {
-        alert("COMPUTER WINS 5 MATCHES FIRST !!!!:");
-    }
-    else if (user == 5)
-    {
-        alert("USER WINS 5 MATCHES FIRST !!!!:");
-    }
-}
-
-function play()
-{
-    play = true;
-    while (play)
-    {
-        rounds();
-        let input = prompt("Want to play again? Y or N only:")
-        {
-            if (input=='Y')
-            {
-                play = true;
-            }
-            else
-            {
-                play = false;
-            }
-        }
-    }
-
-}
-
-play()
