80342d31de5cbb3f890a5b4f5354fd05555997ed
pulsarchief <145131830+pulsarchief@users.noreply.github.com>
2024-07-17 20:19:12 +0530

    everything is done and tested works well logic for reset after 5 rounds not added yet

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
-const user_choice = document.querySelector("#choice_c");
-const comp_choice = document.querySelector("#choice_u");
+const user_choice = document.querySelector("#choice_u"); // user choice image 
+const comp_choice = document.querySelector("#choice_c"); // computer choice image 
 
-const user_score = document.querySelector("#user_score");
-const comp_score = document.querySelector("#comp_score");
-const info = document.querySelector("#info");
+const user_score = document.querySelector("#user_score"); // user score 
+const comp_score = document.querySelector("#comp_score"); // comp score 
+const info = document.querySelector("#info"); // tie , who won etc
 
-const paper = document.querySelector("#paper");
-const stone = document.querySelector("#stone");
-const scissors = document.querySelector("#scissors");
+const stone = document.querySelector("#stone"); //stone button
+const paper = document.querySelector("#paper"); // paper button
+const scissors = document.querySelector("#scissors"); // scissors button 
 
-stone.addEventListener("click", play(1));
-paper.addEventListener("click", play(2));
-scissors.addEventListener("click", play(3));
+const rounds = document.querySelector("#rounds");
+
+
+stone.addEventListener("click", () => play(0)); // clicking stone button
+paper.addEventListener("click", () => play(1)); // clicking paper button
+scissors.addEventListener("click", () => play(2)); // clicking scissors button
+
 
 function play(user) {
-    let choices = ['stone', 'paper', 'scissors'];
-    let user_choice = user;
-    let comp_choice = Math.floor(Math.random() * 3) + 1;
-
-    switch (user_choice) {
-        case 1:
-            switch (comp_choice) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
-        case 2:
-            switch (comp_choice) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
-        case 3:
-            switch (comp_choice) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
-        default:
-            break;
+    let choices = ["stone", "paper", "scissors"];
+    let choices_img = ["icons/stone.png", "icons/paper.png", "icons/scissors.png"];
+    let u = user;
+    let c = Math.floor(Math.random() * 3);
+    user_choice.setAttribute("src", choices_img[u]);
+    comp_choice.setAttribute("src", choices_img[c]);
+    incrementRounds();
+    logic(u, c);
+    if (user_score.textContent >= 5) {
+        info.textContent = "USER WINS 5!!!!"
+        return;
     }
+    if (comp_score.textContent >= 5) {
+        info.textContent = "COMPUTER WINS 5!!!!"
+        return;
+    }
+}
+
+function logic(user, computer) {
+    if (user == computer) {
+        info.textContent = "TIE";
+        return;
+    }
+    if ((user == 0 && computer == 2) || (user == 1 && computer == 0) || (user == 2 && computer == 1)) {
+        info.textContent = "USER WINS";
+        user_score.textContent++;
+    }
+    if ((user == 0 && computer == 1) || (user == 1 && computer == 2) || (user == 2 && computer == 0)) {
+        info.textContent = "COMPUTER WINS";
+        comp_score.textContent++;
+    }
+}
+
+function incrementRounds() {
+    let arr = rounds.textContent;
+    arr = arr.split(" ");
+    arr[1]++;
+    rounds.textContent = arr[0] + " " + arr[1];
 }
